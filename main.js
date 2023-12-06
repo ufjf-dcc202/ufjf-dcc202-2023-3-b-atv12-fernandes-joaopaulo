c9660298fa48586f54f84646a9cca8138a262d36
Joao Paulo <fernandes.joao@estudante.ufjf.br>
2023-12-05 21:43:12 -0300

    Cria evento atualizaTela

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-const olJoao = document.querySelector('ol#joao');
+import { getEstoque } from "./estoque.js";
+
+const olJoao = document.querySelector('#joao');
 const olMaria = document.querySelector('#maria');
 
 document.entrada.addEventListener('submit', leFormulario);
+
+atualizaTela();
 
 function leFormulario(event){
 
@@ -14,7 +18,16 @@ function leFormulario(event){
     console.log(`${origem} doa ${quantidade} ${fruta} para ${destino}`);
 }
 
-
+function atualizaTela(){
+    const estoque = getEstoque();
+    olJoao.innerHTML = "";
+    for(let i=0;i<estoque.joao.length; i++){
+        const monte = estoque.joao[i];
+        const li = document.createElement('li');
+        li.textContent = `${monte.tipo}: ${monte.qtd}`;
+        olJoao.append(li);
+    }
+}
 
 
 
